9351380e9b23fd4ce837f8dda2ceaf65ee7e1b26
Saharsh Shukla <saharsh.shukla2018@vitstudent.ac.in>
2021-07-12 17:15:12 +0530

    Change App Default message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import ReactDOM from 'react-dom';
-
 import './index.css';
 import App from './App';
 
+const ReactDOM = require('react-dom')
+
 ReactDOM.render(<App />, document.getElementById('root'));
+
