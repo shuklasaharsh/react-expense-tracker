664e9f9259bb740f581690ba3bba9e7fb471d286
Saharsh Shukla <saharsh.shukla2018@vitstudent.ac.in>
2021-07-18 22:49:48 +0530

    Add List

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from "./ExpensesList";
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -12,20 +12,17 @@ const Expenses = (props) => {
         setFilteredYear(selectedYear);
     };
 
+    const filteredExpenses = props.items.filter(expense=> expense.date.getFullYear().toString() === filteredYear);
+
+
+
     return (
-        <div>
+        <li>
             <Card className='expenses'>
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-                {props.items.map((item) => {
-                    return <ExpenseItem
-                        key={item.id}
-                        title={item.title}
-                        amount={item.amount}
-                        date={item.date}
-                    />
-                })}
+                <ExpensesList items={filteredExpenses}/>
             </Card>
-        </div>
+        </li>
     );
 };
 
